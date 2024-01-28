4c70d3951d72c4bdfa099671f60a7d983f01c556
Muneebaslam683 <96602060+Muneebaslam683@users.noreply.github.com>
2024-01-28 20:10:13 +0500

    Create structure.lua

---
+++ structure.lua
@@ -0,0 +1,11 @@
+/flask-app
+|-- venv/                   # (Optional) Virtual environment
+|-- static/
+|   |-- style.css           # Example: CSS file
+|-- templates/
+|   |-- index.html          # Example: HTML template
+|-- .gitignore              # Git ignore file
+|-- app.py                  # Flask application
+|-- Procfile                # Specifies how the app should run on PythonAnywhere
+|-- requirements.txt        # List of dependencies
+|-- README.md               # Project documentation
